8cef30aa598f9369ecf1d927d39f9b3c9589f921
Ricky Rivas <rickyrivas918@gmail.com>
2022-08-23 23:37:22 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>home</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
