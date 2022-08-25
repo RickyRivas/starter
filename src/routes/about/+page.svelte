97d131d3afed80cf1f7ff151acb1d7313a10ec46
Ricky Rivas <rickyrivas918@gmail.com>
2022-08-24 23:13:32 -0500

    about page complete

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,20 +12,53 @@
 
 <SubHeader {pageTitle} />
 <main id="about">
-	<div class="content">
-		<h1>About this app</h1>
-
-		<p>
-			This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-			following into your command line and following the prompts:
-		</p>
-
-		<pre>npm create svelte@latest</pre>
-
-		<p>
-			The page you're looking at is purely static HTML, with no client-side interactivity needed.
-			Because of that, we don't need to load any JavaScript. Try viewing the page's source, or
-			opening the devtools network panel and reloading.
-		</p>
+	<div class="container">
+		<div class="img-container">
+			<img
+				class=""
+				src="/about/owner.webp"
+				alt="Owner"
+				width="500"
+				height="750"
+				loading="lazy"
+				decoding="async"
+			/>
+			<div class="name">
+				<p>Richard Smith</p>
+				<span>Owner</span>
+			</div>
+		</div>
+		<div class="content">
+			<div class="flex">
+				<img
+					class=""
+					src="/about/grass.svg"
+					alt=""
+					width="25"
+					height="25"
+					loading="lazy"
+					decoding="async"
+				/>
+				<span>Our Story</span>
+			</div>
+			<h2>Who we are</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quae quo, earum a nobis
+				officiis maiores natus hic omnis corrupti minima eius eaque odio, placeat ad vero magnam
+				pariatur harum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, suscipit
+				laborum est perferendis illo, totam, itaque dolores nulla debitis impedit reiciendis
+				repellendus in id. Consectetur placeat quis dolores voluptatem quibusdam?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quae quo, earum a nobis
+				officiis maiores natus hic omnis corrupti minima eius eaque odio, placeat ad vero magnam
+				pariatur harum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, suscipit
+				laborum est perferendis illo, totam, itaque dolores nulla debitis impedit reiciendis
+				repellendus in id. Consectetur placeat quis dolores voluptatem quibusdam?
+			</p>
+			<div class="btns">
+				<a href="/">Contact Us</a>
+			</div>
+		</div>
 	</div>
 </main>
