5cda0575d29686247de58f6ea954ed0658f048f9
Ricky Rivas <rickyrivas918@gmail.com>
2022-08-24 21:24:42 -0500

    contact page template

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -22,30 +22,24 @@
 			</div>
 			<ul>
 				<li>
-					<a href="/">
-						<img src="/phone-callg.svg" alt="Phone Icon" height="50" width="50" />
-					</a>
+					<img src="/contact/phone-callg.svg" alt="Phone Icon" height="50" width="50" />
 					<div>
 						<p>Give Us a Call</p>
 						<a href="/" class="phone">(918) 123 - 4567</a>
 					</div>
 				</li>
 				<li>
-					<a href="/">
-						<img src="/locationg.svg" alt="Phone Icon" height="50" width="50" />
-					</a>
+					<img src="/contact/locationg.svg" alt="Phone Icon" height="50" width="50" />
 					<div>
 						<p>Our Location</p>
 						<a href="/" class="location">1234 E Street, Tulsa, Oklahoma</a>
 					</div>
 				</li>
 				<li>
-					<a href="/">
-						<img src="/emailg.svg" alt="Phone Icon" height="50" width="50" />
-					</a>
+					<img src="/contact/emailg.svg" alt="Phone Icon" height="50" width="50" />
 					<div>
-						<p>Business Email</p>
-						<a href="/" class="phone">landscaping@gmail.com</a>
+						<p>Business Hours</p>
+						<a href="/" class="phone">Monday - Friday: 8:00am-5:00pm</a>
 					</div>
 				</li>
 			</ul>
@@ -90,5 +84,15 @@
 				<button type="submit" class="btn btn-primary">Send Message</button>
 			</form>
 		</div>
+	</div>
+	<div class="map">
+		<iframe
+			title="Business Location"
+			id="map"
+			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206181.47982743487!2d-95.87801045!3d36.1523015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b692b8ddd12e8f%3A0xe76910c81bd96af7!2sTulsa%2C%20OK!5e0!3m2!1sen!2sus!4v1661391546906!5m2!1sen!2sus"
+			allowfullscreen=""
+			loading="lazy"
+			referrerpolicy="no-referrer-when-downgrade"
+		/>
 	</div>
 </main>
