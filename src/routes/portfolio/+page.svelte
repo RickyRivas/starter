6acf8d1055d9c9ccf349498016ac36c7f1e1a098
Ricky Rivas <rickyrivas918@gmail.com>
2022-08-24 21:59:00 -0500

    portfolio page complete

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -12,5 +12,111 @@
 
 <SubHeader {pageTitle} />
 <main id="portfolio">
-	<h1>Portfolio</h1>
+	<div class="grid">
+		<div class="col">
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img01.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img02.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img03.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+		</div>
+		<div class="col">
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img02.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img01.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img01.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+		</div>
+		<div class="col">
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img01.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img02.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+			<div class="item">
+				<img
+					class=""
+					src="/portfolio/img03.webp"
+					width=""
+					height=""
+					alt=""
+					loading="lazy"
+					decoding="async"
+				/>
+			</div>
+		</div>
+	</div>
 </main>
