3578af670633409e486a03245fc22542e66ca82b
Ricky Rivas <rickyrivas918@gmail.com>
2022-08-27 13:26:55 -0500

    testimonials/misc page tightened up

--- src/routes/testimonials/+page.svelte
+++ src/routes/testimonials/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	// External styles
+	import { fade } from 'svelte/transition';
 	import SubHeader from '../../lib/header/SubHeader.svelte';
 	import './misc.scss';
 	let pageTitle = 'Testimonials';
+	let stars = 5;
 	let reviews = [
 		{
 			testifier: 'Valued Customer',
@@ -45,14 +47,25 @@
 <SubHeader {pageTitle} />
 <main id="services">
 	<div class="container">
-		{#each reviews as { review, testifier }}
-			<div class="item">
+		{#each reviews as { review, testifier }, i}
+			<div class="item" in:fade={{ y: 200, duration: 1000, delay: i * 200 }}>
 				<div class="stars">
-					<img src="/reviews/star.svg" alt="star" width="25" height="25" />
-					<img src="/reviews/star.svg" alt="star" width="25" height="25" />
-					<img src="/reviews/star.svg" alt="star" width="25" height="25" />
-					<img src="/reviews/star.svg" alt="star" width="25" height="25" />
-					<img src="/reviews/star.svg" alt="star" width="25" height="25" />
+					{#each { length: stars } as _}
+						<svg
+							width="30"
+							height="30"
+							viewBox="0 0 30 30"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
+							role="img"
+						>
+							<path
+								d="M30 11.5908L19.0993 10.8741L14.994 0.560669L10.8888 10.8741L0 11.5908L8.3516 18.6821L5.61105 29.4393L14.994 23.5084L24.3771 29.4393L21.6365 18.6821L30 11.5908Z"
+								fill="currentcolor"
+							/>
+						</svg>
+					{/each}
 				</div>
 				<p>
 					{review}
@@ -61,7 +74,7 @@
 					<img src="/reviews/user.svg" alt="testifier" width="40" height="40" />
 					<div>
 						<h4>{testifier}</h4>
-						<p>Certified Google Review</p>
+						<p>Homeowner</p>
 					</div>
 				</div>
 			</div>
