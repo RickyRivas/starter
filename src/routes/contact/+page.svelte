f75ef0d38eb26a69dc7ecf4dd65db53c87ceb60e
Ricky Rivas <rickyrivas918@gmail.com>
2022-09-27 13:03:40 -0500

    new netlify enabled form + design

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,7 +4,7 @@
 	// components/ etc
 	import SubHeader from './../../lib/header/SubHeader.svelte';
 	import './contact.scss';
-	let pageTitle = 'Contact Us';
+	let pageTitle = 'Contact';
 </script>
 
 <svelte:head>
@@ -14,87 +14,89 @@
 <SubHeader {pageTitle} />
 <main id="contact">
 	<div class="container">
-		<div class="info-container">
-			<!-- <span>Contact Us</span> -->
-			<h2>Contact us for your landscaping needs.</h2>
-			<div class="para">
-				<p>
-					Feel free to contact us for any construction services. Give us a call for immediate help.
-				</p>
-			</div>
-			<ul>
-				<li>
-					<img src="/contact/phone-callg.svg" alt="Phone Icon" height="50" width="50" />
-					<div>
-						<p>Give Us a Call</p>
-						<a href="tel:{businessInfo.phone}" class="phone">{businessInfo.phone}</a>
-					</div>
-				</li>
-				<li>
-					<img src="/contact/locationg.svg" alt="Phone Icon" height="50" width="50" />
-					<div>
-						<p>Our Location</p>
-						<a href={businessInfo.googleLink} class="location">{businessInfo.fullAddress}</a>
-					</div>
-				</li>
-				<li>
-					<img src="/contact/emailg.svg" alt="Phone Icon" height="50" width="50" />
-					<div>
-						<p>Business Hours</p>
-						<a href="/" class="phone">Monday - Friday: 8:00am-5:00pm</a>
-					</div>
-				</li>
-			</ul>
-		</div>
 		<div class="form-container">
-			<form name="Contact Form" id="contact-form" method="POST" netlify>
-				<div class="flex">
-					<div class="form-control dropdown">
-						<p>How did you hear about us?</p>
-						<select name="contact-reason" form="contact-form" aria-label="contact reason" required>
-							<option value="" disabled selected>Select an option</option>
-							<option value="option">Option 1</option>
-							<option value="optiontwo">Option 2</option>
-							<option value="option3">Option 3</option>
-						</select>
-					</div>
-					<div class="form-control">
-						<p>Your Name:</p>
-						<input type="text" name="name" aria-label="Your name" placeholder="Name" required />
-					</div>
-				</div>
-				<div class="flex">
-					<div class="form-control">
-						<p>Your Email:</p>
-						<input type="email" name="email" aria-label="Your email" placeholder="Email" required />
-					</div>
-					<div class="form-control">
-						<p>Your Phone:</p>
-						<input
-							type="number"
-							name="number"
-							aria-label="Your phone number"
-							placeholder="Phone"
-							required
-						/>
-					</div>
+			<h2>Get in touch.</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nemo sint tenetur non maxime
+				alias!
+			</p>
+			<form id="contact-form" method="POST" name="contact-form" netlify>
+				<input type="hidden" name="form-name" value="contact-form" />
+
+				<div class="form-control">
+					<!-- -->
+					<label for="fname">Name</label>
+					<input id="fname" type="text" name="Client Name" placeholder="John Doe" required />
 				</div>
 				<div class="form-control">
-					<p>How can we help?</p>
-					<textarea aria-label="message" placeholder="Your Message" name="message" required />
+					<label for="email-input">Email</label>
+					<input id="email-input" type="email" name="Email" placeholder="you@email.com" required />
 				</div>
-				<button type="submit" class="btn btn-primary">Send Message</button>
+				<div class="form-control">
+					<label for="phone-input">Phone Number</label>
+					<input
+						id="phone-input"
+						type="number"
+						name="number"
+						placeholder="+1 (918) 000-0000"
+						required=""
+					/>
+				</div>
+				<div class="form-control">
+					<label for="service-input">How did you hear about us?</label>
+					<input
+						id="service-input"
+						type="text"
+						name="Services"
+						placeholder="Facebook, Referral, Google"
+						required=""
+					/>
+				</div>
+				<div class="form-control">
+					<label for="message-input">How can we help?</label>
+					<textarea
+						id="message-input"
+						name="message"
+						cols="20"
+						rows="5"
+						placeholder="Hey business, I need help with..."
+					/>
+				</div>
+				<button id="submit">
+					<span> Submit Form </span>
+				</button>
+				<!-- -->
 			</form>
 		</div>
-	</div>
-	<div class="map">
-		<iframe
-			title="Business Location"
-			id="map"
-			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206181.47982743487!2d-95.87801045!3d36.1523015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b692b8ddd12e8f%3A0xe76910c81bd96af7!2sTulsa%2C%20OK!5e0!3m2!1sen!2sus!4v1661391546906!5m2!1sen!2sus"
-			allowfullscreen=""
-			loading="lazy"
-			referrerpolicy="no-referrer-when-downgrade"
-		/>
+		<div class="info">
+			<ul>
+				<li>
+					<p>Phone:</p>
+					<a href="/">+1 (918) 000-0000</a>
+				</li>
+				<li>
+					<p>Email:</p>
+					<a href="/">business@email.com</a>
+				</li>
+				<li>
+					<p>Business Hours:</p>
+					<p class="hr">Monday - Friday: 7am - 5pm</p>
+				</li>
+				<li>
+					<p>Business Location:</p>
+					<a href="/">1234 East Tulsa, St. 74110</a>
+				</li>
+			</ul>
+			<div class="map">
+				<iframe
+					title="Business Location"
+					id="map"
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d206181.47982743487!2d-95.87801045!3d36.1523015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b692b8ddd12e8f%3A0xe76910c81bd96af7!2sTulsa%2C%20OK!5e0!3m2!1sen!2sus!4v1661391546906!5m2!1sen!2sus"
+					allowfullscreen=""
+					loading="lazy"
+					referrerpolicy="no-referrer-when-downgrade"
+				/>
+			</div>
+		</div>
 	</div>
 </main>
